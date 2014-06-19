df15624937fce03cf35ab5dbaa2c61f7c13e8c5f
naviens <naviensubramani@gmail.com>
2014-06-19 22:47:10 +0530

    Basic

---
+++ gce/setup.py
@@ -0,0 +1,12 @@
+__author__ = 'naveen'
+from setuptools import setup
+
+setup(
+    name="MyLibrary",
+    version="1.0",
+    install_requires=[
+        "oauth2client",
+        "google-api-python-client",
+    ],
+    # ...
+)
